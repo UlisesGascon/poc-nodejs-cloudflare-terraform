569dc4a41fb441563f0f4074f5c28236e11f80b5
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-06-20 13:38:41 +0200

    fix: variable declaration

---
+++ terraform/variable.tf
@@ -0,0 +1,9 @@
+variable "iojs_zone_id" {
+  type        = string
+  description = "Cloudflare Zone ID for iojs.org"
+}
+
+variable "nodejs_zone_id" {
+  type        = string
+  description = "Cloudflare Zone ID for nodejs.org"
+}
